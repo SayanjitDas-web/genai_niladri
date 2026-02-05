41c47d7c48b5834103ea9395a2df0782827df925
SayanjitDas-web <thewebman01@gmail.com>
2026-02-05 18:22:38 +0530

    class 3 code added 5/2/26

--- main.py
+++ main.py
@@ -23,41 +23,71 @@
 
 # class-2 (3/2/26)
 
-condition = 1 > 2
-target = 10
+# condition = 1 > 2
+# target = 10
 
-if(target > 2 and condition):
-    print("right")
-elif(target == 10 or condition):
-    print("equal")
-else:
-    print("wrong")
+# if(target > 2 and condition):
+#     print("right")
+# elif(target == 10 or condition):
+#     print("equal")
+# else:
+#     print("wrong")
 
-print("right") if(condition) else (print("equal") if (target == 10) else print("wrong"))
+# print("right") if(condition) else (print("equal") if (target == 10) else print("wrong"))
 
-point = 80
+# point = 80
 
-match(point):
-    case 50:
-        print("you got 50 points")
-    case 70:
-        print("you got 70 points")
-    case 90:
-        print("you got 90 points")
-    case _:
-        print("execeptional case")
+# match(point):
+#     case 50:
+#         print("you got 50 points")
+#     case 70:
+#         print("you got 70 points")
+#     case 90:
+#         print("you got 90 points")
+#     case _:
+#         print("execeptional case")
 
-text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto voluptas iste harum iusto recusandae laudantium aperiam mollitia! Eius, quam perspiciatis?"
+# text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto voluptas iste harum iusto recusandae laudantium aperiam mollitia! Eius, quam perspiciatis?"
 
-# for char in text:
-#     print(word)
+# # for char in text:
+# #     print(word)
 
-for i in range(len(text)):
-    if i == 10:
-        break
-    print(text[i])
+# for i in range(len(text)):
+#     if i == 10:
+#         break
+#     print(text[i])
 
 # i = 0 
 # while(i < len(text)):
 #     print(text[i])
 #     i += 1
+
+# class-3 (5/2/26)
+
+# list , dictionary , tuple , set
+
+# myList = ["one", 2 , 3, True, False]
+
+# for data in myList:
+#     print(data)
+
+# for i in range(1,3):
+#     print(myList[i])
+
+# print(myList[::-1])
+
+# for data in myList[:2]:
+#     print(data)
+
+# myDict = {"username":"Niladri","roll":1}
+
+# print(myDict["username"])
+
+# myDict["username"] = "Sayanjit"
+
+# myDict.update({"age":20})
+
+# del myDict["roll"]
+
+# for [k,v] in myDict.items():
+#     print(k,v)
